96603cb7a10205e4bf0ff22e1b21251295beaa9a
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-11 19:24:55 -0400

    created more instance variables based on information provided by api. Display asteroid information on click

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
 let API_KEY = prompt('Please enter your api key from https://api.nasa.gov/')
-
-fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${API_KEY}`)
-    .then(res => res.json())
-    .then(data => {
-        data.near_earth_objects.forEach(obj => {
-            console.log(obj)
-            let asteroid = new Asteroid(obj.name_limited, obj.id);
-            asteroid.createElement()
-        });
-    })
-    .catch(err => {
-        console.log(err);
-    })
-
 let width = window.innerWidth;
 let height = window.innerHeight;
 
@@ -21,10 +7,27 @@ window.onresize = function () {
     height = window.innerHeight;
 }
 
+fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+        data.near_earth_objects.forEach(obj => {
+            console.log(obj)
+            let asteroid = new Asteroid(obj);
+            asteroid.createElement()
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
 class Asteroid {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
+    constructor(obj) {
+        this.id = obj.id;
+        this.name = obj.name_limited;
+        this.fullName = obj.name;
+        this.firstObserved = obj.orbital_data.first_observation_date;
+        this.magnitude = `${obj.absolute_magnitude_h} N`;
+        this.hazardous = obj.is_potentially_hazardous_asteroid;
         this.x = ~~(Math.random() * width);
         this.y = ~~(Math.random() * height);
         
@@ -113,9 +116,34 @@ class Asteroid {
             title.style.display = "none";
         });
         asteroid.addEventListener('click', () => {
-            console.log('clicked')
-            /* do stuff here to display asteroid information */
+            document.getElementById('overlay').style.display = 'block';
+
+            const title = document.getElementById('overlay-title');
+            title.innerText = this.fullName;
+
+            const observed = document.createElement('p');
+            observed.innerText = `First Observed: ${this.firstObserved}`;
+            observed.classList.add('overlay-info');
+
+            const magnitude = document.createElement('p');
+            magnitude.innerText = `Magnitude: ${this.magnitude}`;
+            magnitude.classList.add('overlay-info');
+
+            const hazard = document.createElement('p');
+            hazard.innerHTML = `
+                Potential Danger:
+                <span style="color: ${this.hazardous ? 'red' : 'white'}">${this.hazardous ? 'This asteroid is a potential threat to Earth!' : 'This asteroid is just a passerby, be thankful it has other plans.'}</span>
+            `;
+            hazard.classList.add('overlay-info');
+
+            title.after(hazard);
+            title.after(observed);
+            title.after(magnitude);
         });
+
+        document.getElementById('x-button').addEventListener('click', () => {
+            document.getElementById('overlay').style.display = 'none';
+        })
     }
 
 
