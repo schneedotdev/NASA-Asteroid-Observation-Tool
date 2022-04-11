488bd080657f645b479c5c4d51032cf8116b1cdf
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-11 17:04:12 -0400

    created div that houses both image and a title of the asteroid, changed JS so that the title displays either above or below, depending on the direction the asteroid moves

--- js/main.js
+++ js/main.js
@@ -26,34 +26,81 @@ class Asteroid {
         this.id = id;
         this.x = ~~(Math.random() * width);
         this.y = ~~(Math.random() * height);
+        
+        // let [x, y] = this.startLocation();
+        // this.x = x;
+        // this.y = y;
     }
 
     createElement() {
-        const asteroid = document.createElement('img');
+        const asteroid = document.createElement('div');
         asteroid.classList.add('asteroid');
-        asteroid.setAttribute('id', this.id);
-        asteroid.src = './img/asteroid2.png';
+        
+        // const above = `<h3 id="${this.id}-above" class="asteroid-name">${this.name}</h3>`
+        // const below = `<h3 id="${this.id}-below" class="asteroid-name">${this.name}</h3>`
+
+        const above = document.createElement('h3');
+        above.setAttribute('id', `${this.id}-above`);
+        above.classList.add('asteroid-name');
+        above.style.display = 'none';
+        above.innerText = this.name;
+
+        const below = document.createElement('h3');
+        below.setAttribute('id', `${this.id}-below`);
+        below.classList.add('asteroid-name');
+        below.style.display = 'none';
+        below.innerText = this.name;
+
+        const img = document.createElement('img');
+        img.setAttribute('id', this.id);
+        img.classList.add('asteroid-img');
+        img.src = './img/asteroid.png';
+        img.alt = "image of an asteroid"
 
         const size = (Math.random() * 19) + 41;
-        asteroid.style.width = `${size}px`;
-        asteroid.style.height = `${size}px`;
+        img.style.width = `${size}px`;
+        img.style.height = `${size}px`;
+
+        asteroid.appendChild(above);
+        asteroid.appendChild(img);
+        asteroid.appendChild(below);
+
+        // const asteroid = document.createElement('img');
+        // asteroid.classList.add('asteroid');
+        // asteroid.setAttribute('id', this.id);
+        // asteroid.src = './img/asteroid.png';
+
+        
+        // const img = document.getElementById(`${this.id}`);
+        // const size = (Math.random() * 19) + 41;
+        // img.style.width = `${size}px`;
+        // img.style.height = `${size}px`;
 
         document.getElementById('asteroids').appendChild(asteroid);
 
-        this.move(asteroid)
+        // const size = (Math.random() * 19) + 41;
+        // asteroid.style.width = `${size}px`;
+        // asteroid.style.height = `${size}px`;
+
+        // document.getElementById('asteroids').appendChild(asteroid);
+
+        this.move(asteroid, img)
     }
 
     // Need to make this method private
-    move(asteroid) {
+    move(asteroid, img) {
         let bool = Math.random() < .5
 
         const step = _ => {
             let val = Math.floor(Math.random() * 3) + 1
-            return  bool ? -val : val
+
+            this.setHoverLocation(bool);
+
+            return  bool ? -val : val;
         }
 
         let [x, y, run, rise] = [this.x, this.y, step(), step()]
-        this.setRotation(asteroid, rise, run);
+        this.rotate(img, rise, run);
 
         let moveAsteroid = setInterval(() => {  
             x += run;
@@ -62,7 +109,7 @@ class Asteroid {
             asteroid.style.top = `${y}px`;
             asteroid.style.left = `${x}px`;
         
-            if (x > width || y > height || x < -50 || y < -50) {
+            if(x > width || y > height || x < -50 || y < -50) {
                 x = this.x
                 y = this.y
             }
@@ -72,13 +119,46 @@ class Asteroid {
     }
 
     // Need to make this method private
-    setRotation(asteroid, rise, run) {
+    rotate(img, rise, run) {
         let angle = Math.atan(rise / run) * 100;
 
-        if (Math.sign(rise) === 1)
+        if(Math.sign(rise) === 1)
             angle = angle + 180;
         
         console.log(angle);
-        asteroid.style.transform = `rotate(${angle}deg)`;
+        img.style.transform = `rotate(${angle}deg)`;
     }
+
+    setHoverLocation(bool) {
+        if(bool) {
+            document.getElementById(`${this.id}-below`).style.display = "block";
+        } else {
+            document.getElementById(`${this.id}-above`).style.display = "block";
+        }
+    }
+
+
+    // startLocation() {
+    //     const direction = Math.floor(Math.random() * 4);
+    //     let [x, y] = [0, 0]
+
+    //     switch(direction) {
+    //         case 0: // top
+    //             y = height;
+    //             x = Math.random() * width;
+    //             break;
+    //         case 1: // right
+    //             y = Math.random() * height;
+    //             x = width;
+    //             break;
+    //         case 2: // bottom
+    //             x = Math.random() * width;
+    //             break;
+    //         case 3: // left
+    //             y = Math.random() * height
+    //             break;
+    //     }
+
+    //     return [x, y];
+    // }
 }
